30c2c929efa65af16974ca7606ba12ef294c5358
Tony Fleming <mtony75@gmail.com>
2015-11-30 14:47:18 -0500

    I am not able to tell if the user input is a number or just a string of mixed characters

--- main.go
+++ main.go
@@ -2,8 +2,22 @@ package main
 
 import (
 	"fmt"
-	"strconv"
+//	"strconv"
 )
+
+func prove_int(uinput string) bool{
+	notInt := false
+	for _ , value := range uinput  {
+		switch {
+			case value >= '0' && value <= '9':
+			default :
+//				fmt.Println("Not an Int")
+//				break
+				notInt = true	
+		}
+	}
+	return notInt	
+}
 
 
 func main() {
@@ -15,17 +29,15 @@ func main() {
 	var rawuserNumber string
 	fmt.Printf("Please enter a non negative number: ")
 	fmt.Scanf("%s", &rawuserNumber)
-	for _ , value := range rawuserNumber  {
-		switch {
-			case value >= '0' && value <= '9':
-			default :
-				fmt.Println("Not an Int")
-				break
-		}
+	notInt := false
+	notInt = prove_int(rawuserNumber)
+//	userNumber, err := strconv.Atoi(rawuserNumber)
+// 	fmt.Println(err)
+	if notInt {
+		fmt.Println("You did not enter an Integer")
+	} else {
+		fmt.Println("That is an Integer")
 	}
-	userNumber, err := strconv.Atoi(rawuserNumber)
-	fmt.Println(err)
-	
 	
 	
 	fmt.Println(userNumber)
